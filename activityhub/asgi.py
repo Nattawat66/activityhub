80651254ffbf88dd35c329c5d162a067a323ba51
Nattawat66 <nattawat.ma.66@ubu.ac.th>
2026-04-06 16:24:54 +0700

    ImproperlyConfigured

--- activityhub/asgi.py
+++ activityhub/asgi.py
@@ -12,15 +12,12 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
-import notifications.routing
-import os
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'activityhub.settings')
-
-application = get_asgi_application()
 django_asgi_app = get_asgi_application()
+
+import chat.routing
+import notifications.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
