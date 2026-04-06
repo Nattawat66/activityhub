f707be1d5a827b5ff19c5600d6286cc2dd3d246f
Nattawat66 <nattawat.ma.66@ubu.ac.th>
2026-04-06 21:20:37 +0700

    'Fix-Image-NotFound-2'

--- activityhub/settings.py
+++ activityhub/settings.py
@@ -181,6 +181,7 @@ STORAGES = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') ### เพิ่มใหม่
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
